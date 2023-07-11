940a9aba1cc005e357554e35720f9945396810f8
fatih <faticetinnn@gmail.com>
2023-07-11 15:59:46 +0300

    diziler 3

---
+++ Dizi_3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Dizi_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[20];
+
+            int rnum, nnum = 0, pnum = 0, ncounter = 0, pcounter = 0;
+
+            Random r = new Random();
+
+            for (int i = 0; i < 18; i++)
+
+            {
+
+                rnum = r.Next(-100, 100);
+
+                num[i] = rnum;
+
+                if (rnum < 0) nnum++;
+
+                else pnum++;
+
+            }
+
+            int[] negatif = new int[nnum];
+
+            int[] pozitif = new int[pnum];
+
+            foreach (int i in num)
+
+            {
+
+                if (i < 0)
+
+                {
+
+                    negatif[ncounter] = i;
+
+                    ncounter++;
+
+                }
+
+                else
+
+                {
+
+                    pozitif[pcounter] = i;
+
+                    pcounter++;
+
+                }
+
+            }
+
+            Console.WriteLine("\nÜretilen Sayılar");
+
+            foreach (int i in num)
+
+            {
+
+                Console.Write(" {0} ", i);
+
+            }
+
+            Console.WriteLine("\nÜretilen Sayılardan Negatif Olanlar");
+
+            foreach (int i in negatif)
+
+            {
+
+                Console.Write(" {0} ", i);
+
+            }
+
+            Console.WriteLine("\nÜretilen Sayılardan Pozitif Olanlar");
+
+            foreach (int i in pozitif)
+
+            {
+
+                Console.Write(" {0} ", i);
+
+            }
+
+            Console.ReadKey();
+
+        }
+
+    } 
+    
+}
---
+++ Diziler 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diziler_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numaric = new int[] { 1, 2, 3, 4, 5 };
+            string[] name = new string[] { "Fatih", "Hakan", "Furkan", "Yasin", "Mertcan" };
+            string[] phone = new string[] { "0542-534-2323", "0532-037-0261", "0535-899-2389", "0534-999-7809", "0342-938-2832" };
+            string[] email = new string[] { "fatcet@gmail.com", "hakan@gmail.com", "furkan@gmail.com", "yasin@gmail.com", "mertcan@gmail.com" };
+
+            int employee;
+            Console.WriteLine("Aradığınız Personel Numarası");
+            employee = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Personel numarası = " + numaric[employee - 1]);
+            Console.WriteLine("Personel adı = " + name[employee - 1]);
+            Console.WriteLine("Personel telefon  numarası = " + phone[employee - 1]);
+            Console.WriteLine("Personel emali = " + email[employee - 1]);
+
+            Console.ReadKey(); 
+        }
+    }
+}

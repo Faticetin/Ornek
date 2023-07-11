bc5ae5a7e7c38ca311e98f4f31865c262077961c
fatih <faticetinnn@gmail.com>
2023-07-11 09:59:56 +0300

    Örnek1

---
+++ Öernek1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Öernek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Console.WriteLine("Metini Giriniz");
+            text= Console.ReadLine();
+
+            foreach (var item in text)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}

81b45eba8bc3b8f747e07ecfb4f26dd08ae606a0
fatih <faticetinnn@gmail.com>
2023-07-12 12:26:13 +0300

    Metotlar 1

--- Dizi_3/Program.cs
+++ Dizi_3/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
@@ -12,90 +13,110 @@ namespace Dizi_3
     {
         static void Main(string[] args)
         {
-            int[] num = new int[20];
+            int positiveNumbersCount = 0;
+            int negativeNumbersCount = 0;
+            int[] numbers = new int[20];
+            GenerateRandomNumbers(numbers);
 
-            int rnum, nnum = 0, pnum = 0, ncounter = 0, pcounter = 0;
+            positiveNumbersCount = GetPositiveNumbersCount(numbers);
+            int[] positiveNumbers = new int[positiveNumbersCount];
 
-            Random r = new Random();
+            negativeNumbersCount = GetNegativeNumbersCount(numbers);
+            int[] negativeNumbers = new int[negativeNumbersCount];
 
-            for (int i = 0; i < 18; i++)
+            SetPositiveNumbers(numbers, positiveNumbers);
+            SetNegativeNumbers(numbers, negativeNumbers);
 
+            Console.WriteLine("Üretilen Sayılar");
+            for(int i = 0; i < numbers.Length; i++)
             {
-
-                rnum = r.Next(-100, 100);
-
-                num[i] = rnum;
-
-                if (rnum < 0) nnum++;
-
-                else pnum++;
-
+                Console.Write(numbers[i] + " ");
             }
 
-            int[] negatif = new int[nnum];
-
-            int[] pozitif = new int[pnum];
-
-            foreach (int i in num)
-
+            Console.Write("\n"+"Pozitif Sayılar " +"\n");
+            foreach (int positiveNumber in positiveNumbers)
             {
-
-                if (i < 0)
-
-                {
-
-                    negatif[ncounter] = i;
-
-                    ncounter++;
-
-                }
-
-                else
-
-                {
-
-                    pozitif[pcounter] = i;
-
-                    pcounter++;
-
-                }
-
+                Console.Write(positiveNumber + " ");
             }
 
-            Console.WriteLine("\nÜretilen Sayılar");
-
-            foreach (int i in num)
-
+            Console.Write("\n"+"Negatif Sayılars" +"\n");
+            foreach (int negativeNumber in negativeNumbers)
             {
-
-                Console.Write(" {0} ", i);
-
+                Console.Write(negativeNumber + " ");
             }
 
-            Console.WriteLine("\nÜretilen Sayılardan Negatif Olanlar");
 
-            foreach (int i in negatif)
-
-            {
-
-                Console.Write(" {0} ", i);
-
-            }
-
-            Console.WriteLine("\nÜretilen Sayılardan Pozitif Olanlar");
-
-            foreach (int i in pozitif)
-
-            {
-
-                Console.Write(" {0} ", i);
-
-            }
 
             Console.ReadKey();
-
         }
 
-    } 
-    
+
+        static void GenerateRandomNumbers(int[] numbers)
+        {
+            Random ran = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = ran.Next(-100, 100);
+            }
+        }
+
+        static int GetPositiveNumbersCount(int[] numbers)
+        {
+            int count = 0;
+            foreach (int number in numbers)
+            {
+                if (number > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        static int GetNegativeNumbersCount(int[] numbers)
+        {
+            int count = 0;
+            foreach (int number in numbers)
+            {
+                if (number < 0)
+                {
+                    count++;
+                }
+
+            }
+            return count;
+        }
+
+        static void SetPositiveNumbers(int[] numbers, int[] positiveNumbers)
+        {
+            int index = 0;
+            foreach(int number in numbers)
+            {
+                if(number > 0)
+                {
+                    positiveNumbers[index] = number;
+                    index++;
+                }
+            }
+        }
+
+        static void SetNegativeNumbers(int[] numbers, int[] negativeNumbers)
+        {
+            int index = 0;
+            foreach(int number in numbers)
+            {
+                if (number < 0)
+                {
+                    negativeNumbers[index] = number;
+                    index++;
+                }
+            }
+        }
+
+    }
+
+
+
 }
